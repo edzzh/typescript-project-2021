37242a6c994a189be80a18617c96d2d377d194a9
Edgars Hartmanis <hartmaniss.edgars@gmail.com>
2022-01-09 20:34:25 +0200

    Modified filter() method from lodash module as a module augmentation example

---
+++ src/types/lodash/index.d.ts
@@ -0,0 +1,3 @@
+declare module 'lodash' {
+    export function filter(obj: (string | number | object)[] | object, func: string | (string | number | object)[] | object): (string | number | object)[];
+}
